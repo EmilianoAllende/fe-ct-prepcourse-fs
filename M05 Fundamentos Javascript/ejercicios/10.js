09ddd69b457196115e4c78fc172172642acc7c95
EmilianoAllende <emilianoallende94@gmail.com>
2024-01-23 01:45:54 -0300

    "Henry PrepCourse Homework for Modulo 05"

--- M05 Fundamentos Javascript/ejercicios/10.js
+++ M05 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,15 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+
+       // var fechaDeHoy = new(Date);
+  if(fecha.toString() == 'Invalid Date') {
+    return false
+  } else if(fecha instanceof Date) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = esFechaValida;
